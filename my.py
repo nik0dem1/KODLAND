4d0f1f5288bb1f9b3ff732a887fbb706d0ed8ca7
nik0dem1 <axolszydlowski54@gmail.com>
2024-09-03 19:26:58 +0200

    Create my.py
    my first code

---
+++ my.py
@@ -0,0 +1,10 @@
+slowa = {
+    "LOL": "odpowiedź na coś zabawnego",
+    "CRINGE": "coś dziwnego lub wstydliwego",
+    "ROFL": "odpowiedź na żart",
+    "SHEESH": "lekka dezaprobata",
+    "CREEPY": "straszny, złowieszczy",
+    "AGGRO" : "stać się agresywnym/zły",
+}
+slowo = input("podaj słowo")
+print(slowa[slowo])
